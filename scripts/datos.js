c2580c288ce402b68fdca7b208c6769b1428f328
desarrolloecteam <desarrollo@ecteam.mx>
2024-05-23 22:22:52 -0600

    Initial commit

---
+++ scripts/datos.js
@@ -0,0 +1,201 @@
+rondas = [
+  {
+    /***** RONDA 1 *****/
+
+    preguntas: [
+      {
+        /* Puntos normales */
+        nombre: '¿Qué materia te gustaría que usara inteligencia artificial para mejorar la enseñanza?',
+        respuestas: [
+          { nombre: 'Matemáticas', numero: 40 },
+          { nombre: 'Ciencias', numero: 30 },
+          { nombre: 'Historia', numero: 15 },
+          { nombre: 'Idiomas', numero: 10 },
+          { nombre: 'Arte', numero: 5 }
+        ]
+      },
+
+      {
+        /* Puntos normales */
+        nombre: '¿Qué aplicación de IA te gustaría ver en tu escuela?',
+        respuestas: [
+          { nombre: 'Tutor Inteligente', numero: 35 },
+          { nombre: 'Sistema de calificaciones automático', numero: 25},
+          { nombre: 'Asistente de tareas', numero: 20 },
+          { nombre: 'Planificación de horarios', numero: 15 },
+          { nombre: 'Análisis de rendimiento', numero: 5 }
+        ]
+      },
+
+      {
+        /* Puntos normales */
+        nombre: '¿Qué habilidad crees que necesitas para entender IA?',
+        respuestas: [
+          { nombre: 'Programación', numero: 45 },
+          { nombre: 'Matemáticas', numero: 25 },
+          { nombre: 'Lógica', numero: 15 },
+          { nombre: 'Análistas de Datos', numero: 10 },
+          { nombre: 'Estadística', numero: 5 },
+        ]
+      },
+
+      {
+        /* Puntos normales */
+        nombre: '¿Qué red social crees que utiliza IA de forma impresionante?',
+        respuestas: [
+          { nombre: 'Instagram', numero: 35 },
+          { nombre: 'TikTok', numero: 30 },
+          { nombre: 'Facebook', numero: 20 },
+          { nombre: 'Twitter', numero: 10 },
+          { nombre: 'Snapchat', numero: 5 },
+        ]
+      },
+
+      {
+        /* Puntos normales */
+        nombre: '¿Qué herramienta de IA te gustaría usar para estudiar?',
+        respuestas: [
+          { nombre: 'ChatGPT', numero: 40 },
+          { nombre: 'WolframAlpha', numero: 25 },
+          { nombre: 'Duolingo', numero: 20 },
+          { nombre: 'Grammarly', numero: 10 },
+          { nombre: 'Quizlet', numero: 5 },
+        ]
+      },
+
+      {
+        /* Puntos normales */
+        nombre: '¿Qué aspecto de la vida diaria te gustaría mejorar con IA?',
+        respuestas: [
+          { nombre: 'Educación', numero: 30 },
+          { nombre: 'Salud', numero: 25 },
+          { nombre: 'Transporte', numero: 20 },
+          { nombre: 'Entretenimiento', numero: 15 },
+          { nombre: 'Hogar', numero: 10 },
+        ]
+      }
+    ]
+  },
+
+  /***** RONDA 2 *****/
+
+  {
+    preguntas: [
+      {
+        nombre: '¿Qué videojuego famoso usa IA de manera avanzada?',
+        respuestas: [
+          { nombre: 'Fortnite', numero: 30 },
+          { nombre: 'FIFA', numero: 25 },
+          { nombre: 'Call Of Duty', numero: 20 },
+          { nombre: 'The Sims', numero: 15 },
+          { nombre: 'Minecraft', numero: 10 }
+        ]
+      },
+      {
+        nombre: '¿Qué película sobre IA te parece más interesante?',
+        respuestas: [
+          { nombre: 'Matrix', numero: 35 },
+          { nombre: 'Im Robot', numero: 25 },
+          { nombre: 'Her', numero: 20 },
+          { nombre: 'Ex Machina', numero: 15 },
+          { nombre: 'Terminator', numero: 5 }
+        ]
+      },
+      {
+        nombre: '¿Cuál es el mayor desafío de la IA en el futuro cercano?',
+        respuestas: [
+          { nombre: 'Ética', numero: 30 },
+          { nombre: 'Seguridad', numero: 25 },
+          { nombre: 'Privacidad', numero: 20 },
+          { nombre: 'Pérdida de empleos', numero: 15 },
+          { nombre: 'Integración Social', numero: 10 }
+        ]
+      },
+
+      {
+        nombre: '¿Qué dispositivo te gustaría que tuviera más funciones de IA?',
+        respuestas: [
+          { nombre: 'Teléfono móvil', numero: 45 },
+          { nombre: 'Computadora portátil', numero: 25 },
+          { nombre: 'Tablet', numero: 15 },
+          { nombre: 'Televisor', numero: 10 },
+          { nombre: 'Reloj Inteligente', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Qué sector crees que tendrá más empleos relacionados con IA?',
+        respuestas: [
+          { nombre: 'Tecnologia', numero: 40 },
+          { nombre: 'Salud', numero: 25 },
+          { nombre: 'Finanzas', numero: 15 },
+          { nombre: 'Educación', numero: 10 },
+          { nombre: 'Transporte', numero: 10 }
+        ]
+      },
+
+      {
+        nombre: '¿Qué país crees que está liderando en investigaciones de IA?',
+        respuestas: [
+          { nombre: 'Estados Unidos', numero: 50 },
+          { nombre: 'China', numero: 30 },
+          { nombre: 'Japón', numero: 10 },
+          { nombre: 'Alemania', numero: 5 },
+          { nombre: 'Canadá', numero: 5 }
+        ]
+      },
+
+      
+      
+    ]
+  },
+
+  /***** RONDA 3 *****/
+
+  {
+    preguntas: [
+      {
+        nombre: 'Habilidad de un administrador',
+        respuestas: [
+          { nombre: 'Toma de decisiones', numero: 59 },
+          { nombre: 'Comunicacion', numero: 8 },
+          { nombre: 'Proactividad', numero: 14 },
+          { nombre: 'Eficiencia', numero: 8 },
+          { nombre: 'Perseverancia', numero: 8 }
+        ]
+      },
+      {
+        nombre: '¿Cuál es profesor con mayor tiempo impartiendo clases en el TESVG?',
+        respuestas: [
+          { nombre: 'L.D Alberto Isac Millán Pérez', numero: 30 },
+          { nombre: 'L.C. Brenda Merit Zariñana Beltrán', numero: 25 },
+          { nombre: 'M.en E. Carolina Araujo Vargas', numero: 19 },
+          { nombre: 'Mtra. Anabel Regina Montes de OcaEstrada', numero: 16 },
+          { nombre: 'M.A.D.N Esperanza Segura Figueroa', numero: 10 }
+        ]
+      },
+      {
+        nombre: 'Dias no laborales en el Estado de Mexico',
+        respuestas: [
+          { nombre: '5 de Mayo', numero: 38 },
+          { nombre: '1 de Enero', numero: 30 },
+          { nombre: '12 de Diciembre', numero: 15 },
+          { nombre: '1 de Mayo', numero: 14 },
+          { nombre: '2 de Marzo', numero: 3 }
+        ]
+      },
+      {
+        nombre: '¿Cuáles son las actividades que se realizan en la semana de la administración?',
+        respuestas: [
+          { nombre: 'Conferencias', numero: 56 },
+          { nombre: 'Cena', numero: 19 },
+          { nombre: 'Actividades recreativas', numero: 13 },
+          { nombre: 'Concurso de Rey y Reina', numero: 6 },
+          { nombre: 'Torneos', numero: 6 }
+        ]
+      }
+    ]
+  },
+
+
+];
