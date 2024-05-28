0d03e8adcc0e08f221f28809c81cc20d3530c2d0
luizX21 <210698jldd@gmail.com>
2024-05-27 21:13:14 -0600

    Preguntas

--- scripts/datos.js
+++ scripts/datos.js
@@ -1,201 +1,362 @@
 rondas = [
-  {
-    /***** RONDA 1 *****/
 
+  /***** RONDA 1 *****/
+  {
     preguntas: [
       {
-        /* Puntos normales */
-        nombre: '¿Qué materia te gustaría que usara inteligencia artificial para mejorar la enseñanza?',
+        nombre: '¿En qué área crees que la inteligencia artificial tendrá el mayor impacto en el futuro?',
         respuestas: [
-          { nombre: 'Matemáticas', numero: 40 },
-          { nombre: 'Ciencias', numero: 30 },
-          { nombre: 'Historia', numero: 15 },
-          { nombre: 'Idiomas', numero: 10 },
-          { nombre: 'Arte', numero: 5 }
-        ]
-      },
-
-      {
-        /* Puntos normales */
-        nombre: '¿Qué aplicación de IA te gustaría ver en tu escuela?',
-        respuestas: [
-          { nombre: 'Tutor Inteligente', numero: 35 },
-          { nombre: 'Sistema de calificaciones automático', numero: 25},
-          { nombre: 'Asistente de tareas', numero: 20 },
-          { nombre: 'Planificación de horarios', numero: 15 },
-          { nombre: 'Análisis de rendimiento', numero: 5 }
-        ]
-      },
-
-      {
-        /* Puntos normales */
-        nombre: '¿Qué habilidad crees que necesitas para entender IA?',
-        respuestas: [
-          { nombre: 'Programación', numero: 45 },
-          { nombre: 'Matemáticas', numero: 25 },
-          { nombre: 'Lógica', numero: 15 },
-          { nombre: 'Análistas de Datos', numero: 10 },
-          { nombre: 'Estadística', numero: 5 },
-        ]
-      },
-
-      {
-        /* Puntos normales */
-        nombre: '¿Qué red social crees que utiliza IA de forma impresionante?',
-        respuestas: [
-          { nombre: 'Instagram', numero: 35 },
-          { nombre: 'TikTok', numero: 30 },
-          { nombre: 'Facebook', numero: 20 },
-          { nombre: 'Twitter', numero: 10 },
-          { nombre: 'Snapchat', numero: 5 },
-        ]
-      },
-
-      {
-        /* Puntos normales */
-        nombre: '¿Qué herramienta de IA te gustaría usar para estudiar?',
-        respuestas: [
-          { nombre: 'ChatGPT', numero: 40 },
-          { nombre: 'WolframAlpha', numero: 25 },
-          { nombre: 'Duolingo', numero: 20 },
-          { nombre: 'Grammarly', numero: 10 },
-          { nombre: 'Quizlet', numero: 5 },
-        ]
-      },
-
-      {
-        /* Puntos normales */
-        nombre: '¿Qué aspecto de la vida diaria te gustaría mejorar con IA?',
-        respuestas: [
-          { nombre: 'Educación', numero: 30 },
-          { nombre: 'Salud', numero: 25 },
+          { nombre: 'Medicina', numero: 35 },
+          { nombre: 'Educación', numero: 25 },
           { nombre: 'Transporte', numero: 20 },
           { nombre: 'Entretenimiento', numero: 15 },
-          { nombre: 'Hogar', numero: 10 },
+          { nombre: 'Finanzas', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el país más grande del mundo por superficie?',
+        respuestas: [
+          { nombre: 'Rusia', numero: 45 },
+          { nombre: 'Canadá', numero: 25 },
+          { nombre: 'China', numero: 15 },
+          { nombre: 'Estados Unidos', numero: 10 },
+          { nombre: 'Brasil', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es la red social más popular actualmente?',
+        respuestas: [
+          { nombre: 'Facebook', numero: 40 },
+          { nombre: 'Instagram', numero: 30 },
+          { nombre: 'TikTok', numero: 20 },
+          { nombre: 'Twitter', numero: 5 },
+          { nombre: 'LinkedIn', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el platillo más representativo de la gastronomía mexicana?',
+        respuestas: [
+          { nombre: 'Tacos', numero: 50 },
+          { nombre: 'Enchiladas', numero: 20 },
+          { nombre: 'Tamales', numero: 15 },
+          { nombre: 'Mole', numero: 10 },
+          { nombre: 'Pozole', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Quién escribió la obra "Don Quijote de la Mancha"?',
+        respuestas: [
+          { nombre: 'Miguel de Cervantes', numero: 70 },
+          { nombre: 'Gabriel García Márquez', numero: 10 },
+          { nombre: 'Julio Cortázar', numero: 10 },
+          { nombre: 'Jorge Luis Borges', numero: 5 },
+          { nombre: 'Mario Vargas Llosa', numero: 5 }
         ]
       }
     ]
   },
 
   /***** RONDA 2 *****/
-
   {
     preguntas: [
       {
-        nombre: '¿Qué videojuego famoso usa IA de manera avanzada?',
+        nombre: '¿Cuál es la mayor preocupación sobre el desarrollo de la inteligencia artificial?',
         respuestas: [
-          { nombre: 'Fortnite', numero: 30 },
-          { nombre: 'FIFA', numero: 25 },
-          { nombre: 'Call Of Duty', numero: 20 },
-          { nombre: 'The Sims', numero: 15 },
-          { nombre: 'Minecraft', numero: 10 }
-        ]
-      },
-      {
-        nombre: '¿Qué película sobre IA te parece más interesante?',
-        respuestas: [
-          { nombre: 'Matrix', numero: 35 },
-          { nombre: 'Im Robot', numero: 25 },
-          { nombre: 'Her', numero: 20 },
-          { nombre: 'Ex Machina', numero: 15 },
-          { nombre: 'Terminator', numero: 5 }
-        ]
-      },
-      {
-        nombre: '¿Cuál es el mayor desafío de la IA en el futuro cercano?',
-        respuestas: [
-          { nombre: 'Ética', numero: 30 },
-          { nombre: 'Seguridad', numero: 25 },
-          { nombre: 'Privacidad', numero: 20 },
-          { nombre: 'Pérdida de empleos', numero: 15 },
-          { nombre: 'Integración Social', numero: 10 }
+          { nombre: 'Pérdida de empleos', numero: 40 },
+          { nombre: 'Privacidad', numero: 25 },
+          { nombre: 'Ética', numero: 20 },
+          { nombre: 'Seguridad', numero: 10 },
+          { nombre: 'Control humano', numero: 5 }
         ]
       },
 
       {
-        nombre: '¿Qué dispositivo te gustaría que tuviera más funciones de IA?',
+        nombre: '¿Cuál es el océano más grande del mundo?',
         respuestas: [
-          { nombre: 'Teléfono móvil', numero: 45 },
-          { nombre: 'Computadora portátil', numero: 25 },
-          { nombre: 'Tablet', numero: 15 },
-          { nombre: 'Televisor', numero: 10 },
-          { nombre: 'Reloj Inteligente', numero: 5 }
+          { nombre: 'Pacífico', numero: 60 },
+          { nombre: 'Atlántico', numero: 20 },
+          { nombre: 'Índico', numero: 10 },
+          { nombre: 'Antártico', numero: 5 },
+          { nombre: 'Ártico', numero: 5 }
         ]
       },
 
       {
-        nombre: '¿Qué sector crees que tendrá más empleos relacionados con IA?',
+        nombre: '¿Qué empresa creó el sistema operativo Windows?',
         respuestas: [
-          { nombre: 'Tecnologia', numero: 40 },
-          { nombre: 'Salud', numero: 25 },
-          { nombre: 'Finanzas', numero: 15 },
-          { nombre: 'Educación', numero: 10 },
-          { nombre: 'Transporte', numero: 10 }
+          { nombre: 'Microsoft', numero: 80 },
+          { nombre: 'Apple', numero: 10 },
+          { nombre: 'Google', numero: 5 },
+          { nombre: 'IBM', numero: 3 },
+          { nombre: 'Dell', numero: 2 }
         ]
       },
 
       {
-        nombre: '¿Qué país crees que está liderando en investigaciones de IA?',
+        nombre: '¿Cuál es el evento histórico más importante de México?',
         respuestas: [
-          { nombre: 'Estados Unidos', numero: 50 },
-          { nombre: 'China', numero: 30 },
-          { nombre: 'Japón', numero: 10 },
-          { nombre: 'Alemania', numero: 5 },
-          { nombre: 'Canadá', numero: 5 }
+          { nombre: 'Independencia de México', numero: 50 },
+          { nombre: 'Revolución Mexicana', numero: 30 },
+          { nombre: 'Conquista de México', numero: 10 },
+          { nombre: 'Guerra de Reforma', numero: 5 },
+          { nombre: 'Porfiriato', numero: 5 }
         ]
       },
 
-      
-      
-    ]
-  },
-
-  /***** RONDA 3 *****/
-
-  {
-    preguntas: [
       {
-        nombre: 'Habilidad de un administrador',
+        nombre: '¿Cuál es el planeta más cercano al Sol?',
         respuestas: [
-          { nombre: 'Toma de decisiones', numero: 59 },
-          { nombre: 'Comunicacion', numero: 8 },
-          { nombre: 'Proactividad', numero: 14 },
-          { nombre: 'Eficiencia', numero: 8 },
-          { nombre: 'Perseverancia', numero: 8 }
-        ]
-      },
-      {
-        nombre: '¿Cuál es profesor con mayor tiempo impartiendo clases en el TESVG?',
-        respuestas: [
-          { nombre: 'L.D Alberto Isac Millán Pérez', numero: 30 },
-          { nombre: 'L.C. Brenda Merit Zariñana Beltrán', numero: 25 },
-          { nombre: 'M.en E. Carolina Araujo Vargas', numero: 19 },
-          { nombre: 'Mtra. Anabel Regina Montes de OcaEstrada', numero: 16 },
-          { nombre: 'M.A.D.N Esperanza Segura Figueroa', numero: 10 }
-        ]
-      },
-      {
-        nombre: 'Dias no laborales en el Estado de Mexico',
-        respuestas: [
-          { nombre: '5 de Mayo', numero: 38 },
-          { nombre: '1 de Enero', numero: 30 },
-          { nombre: '12 de Diciembre', numero: 15 },
-          { nombre: '1 de Mayo', numero: 14 },
-          { nombre: '2 de Marzo', numero: 3 }
-        ]
-      },
-      {
-        nombre: '¿Cuáles son las actividades que se realizan en la semana de la administración?',
-        respuestas: [
-          { nombre: 'Conferencias', numero: 56 },
-          { nombre: 'Cena', numero: 19 },
-          { nombre: 'Actividades recreativas', numero: 13 },
-          { nombre: 'Concurso de Rey y Reina', numero: 6 },
-          { nombre: 'Torneos', numero: 6 }
+          { nombre: 'Mercurio', numero: 50 },
+          { nombre: 'Venus', numero: 20 },
+          { nombre: 'Tierra', numero: 15 },
+          { nombre: 'Marte', numero: 10 },
+          { nombre: 'Júpiter', numero: 5 }
         ]
       }
     ]
   },
 
+  /***** RONDA 3 *****/
+  {
+    preguntas: [
+      {
+        nombre: '¿Qué tipo de IA es capaz de aprender y mejorar por sí misma?',
+        respuestas: [
+          { nombre: 'Aprendizaje profundo (Deep Learning)', numero: 40 },
+          { nombre: 'Aprendizaje automático (Machine Learning)', numero: 30 },
+          { nombre: 'Redes neuronales', numero: 20 },
+          { nombre: 'Algoritmos genéticos', numero: 5 },
+          { nombre: 'Lógica difusa', numero: 5 }
+        ]
+      },
 
+      {
+        nombre: '¿Cuál es la capital de Francia?',
+        respuestas: [
+          { nombre: 'París', numero: 90 },
+          { nombre: 'Marsella', numero: 5 },
+          { nombre: 'Lyon', numero: 3 },
+          { nombre: 'Toulouse', numero: 1 },
+          { nombre: 'Niza', numero: 1 }
+        ]
+      },
+
+      {
+        nombre: '¿Qué es el "Internet de las cosas" (IoT)?',
+        respuestas: [
+          { nombre: 'Conexión de dispositivos', numero: 50 },
+          { nombre: 'Red social', numero: 20 },
+          { nombre: 'Navegador de Internet', numero: 15 },
+          { nombre: 'Motor de búsqueda', numero: 10 },
+          { nombre: 'Aplicación móvil', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es la ciudad más poblada de México?',
+        respuestas: [
+          { nombre: 'Ciudad de México', numero: 60 },
+          { nombre: 'Guadalajara', numero: 20 },
+          { nombre: 'Monterrey', numero: 15 },
+          { nombre: 'Puebla', numero: 3 },
+          { nombre: 'Tijuana', numero: 2 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el idioma más hablado en el mundo?',
+        respuestas: [
+          { nombre: 'Mandarín', numero: 40 },
+          { nombre: 'Inglés', numero: 30 },
+          { nombre: 'Español', numero: 20 },
+          { nombre: 'Hindi', numero: 5 },
+          { nombre: 'Árabe', numero: 5 }
+        ]
+      }
+    ]
+  },
+
+  /***** RONDA 4 *****/
+  {
+    preguntas: [
+      {
+        nombre: '¿Cuál es la aplicación más común de la inteligencia artificial hoy en día?',
+        respuestas: [
+          { nombre: 'Asistentes virtuales (Alexa, Siri)', numero: 40 },
+          { nombre: 'Recomendaciones de productos', numero: 30 },
+          { nombre: 'Reconocimiento facial', numero: 20 },
+          { nombre: 'Diagnóstico médico', numero: 5 },
+          { nombre: 'Vehículos autónomos', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el metal más abundante en la corteza terrestre?',
+        respuestas: [
+          { nombre: 'Aluminio', numero: 40 },
+          { nombre: 'Hierro', numero: 30 },
+          { nombre: 'Calcio', numero: 15 },
+          { nombre: 'Sodio', numero: 10 },
+          { nombre: 'Magnesio', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el lenguaje de programación más utilizado actualmente?',
+        respuestas: [
+          { nombre: 'Python', numero: 35 },
+          { nombre: 'JavaScript', numero: 25 },
+          { nombre: 'Java', numero: 20 },
+          { nombre: 'C++', numero: 10 },
+          { nombre: 'C#', numero: 10 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el destino turístico más popular en México?',
+        respuestas: [
+          { nombre: 'Cancún', numero: 50 },
+          { nombre: 'Ciudad de México', numero: 20 },
+          { nombre: 'Puerto Vallarta', numero: 15 },
+          { nombre: 'Guadalajara', numero: 10 },
+          { nombre: 'Los Cabos', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es la ciencia que estudia los fósiles?',
+        respuestas: [
+          { nombre: 'Paleontología', numero: 70 },
+          { nombre: 'Arqueología', numero: 15 },
+          { nombre: 'Geología', numero: 10 },
+          { nombre: 'Antropología', numero: 3 },
+          { nombre: 'Biología', numero: 2 }
+        ]
+      }
+    ]
+  },
+
+  /***** RONDA 5 *****/
+  {
+    preguntas: [
+      {
+        nombre: '¿Qué compañía es conocida por su asistente de inteligencia artificial llamado "Alexa"?',
+        respuestas: [
+          { nombre: 'Amazon', numero: 80 },
+          { nombre: 'Google', numero: 10 },
+          { nombre: 'Apple', numero: 5 },
+          { nombre: 'Microsoft', numero: 3 },
+          { nombre: 'Samsung', numero: 2 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el autor de "Harry Potter"?',
+        respuestas: [
+          { nombre: 'J.K. Rowling', numero: 60 },
+          { nombre: 'J.R.R. Tolkien', numero: 20 },
+          { nombre: 'George R.R. Martin', numero: 10 },
+          { nombre: 'Suzanne Collins', numero: 5 },
+          { nombre: 'Stephen King', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Qué tipo de tecnología utiliza una impresora 3D?',
+        respuestas: [
+          { nombre: 'Adición de capas', numero: 50 },
+          { nombre: 'Corte por láser', numero: 20 },
+          { nombre: 'Sublimación', numero: 15 },
+          { nombre: 'Escaneo 3D', numero: 10 },
+          { nombre: 'Litografía', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Qué figura histórica es conocida como "El padre de la patria" en México?',
+        respuestas: [
+          { nombre: 'Miguel Hidalgo y Costilla', numero: 70 },
+          { nombre: 'Benito Juárez', numero: 15 },
+          { nombre: 'Emiliano Zapata', numero: 10 },
+          { nombre: 'Pancho Villa', numero: 3 },
+          { nombre: 'Porfirio Díaz', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el animal terrestre más rápido?',
+        respuestas: [
+          { nombre: 'Guepardo', numero: 60 },
+          { nombre: 'León', numero: 20 },
+          { nombre: 'Caballo', numero: 10 },
+          { nombre: 'Antílope', numero: 5 },
+          { nombre: 'Liebre', numero: 5 }
+        ]
+      }
+    ]
+  },
+
+  /***** RONDA 6 *****/
+  {
+    preguntas: [
+      {
+        nombre: '¿Qué película famosa presenta un mundo controlado por inteligencia artificial?',
+        respuestas: [
+          { nombre: 'The Matrix', numero: 50 },
+          { nombre: 'Terminator', numero: 30 },
+          { nombre: 'Blade Runne', numero: 10 },
+          { nombre: 'Ex Machina', numero: 5 },
+          { nombre: 'Her', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Cuál es el río más largo del mundo?',
+        respuestas: [
+          { nombre: 'Amazonas', numero: 50 },
+          { nombre: 'Nilo', numero: 30 },
+          { nombre: 'Yangtsé', numero: 10 },
+          { nombre: 'Misisipi', numero: 5 },
+          { nombre: 'Yeniséi', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Qué es un "bit" en informática?',
+        respuestas: [
+          { nombre: 'Unidad básica de información', numero: 50 },
+          { nombre: 'Unidad de medida de almacenamiento', numero: 25 },
+          { nombre: 'Unidad de medida de velocidad', numero: 10 },
+          { nombre: 'Unidad de medida de tiempo', numero: 10 },
+          { nombre: 'Unidad de medida de energía', numero: 5 }
+        ]
+      },
+
+      {
+        nombre: '¿Qué día se celebra la independencia de México?',
+        respuestas: [
+          { nombre: '16 de septiembre', numero: 80 },
+          { nombre: '5 de mayo', numero: 10 },
+          { nombre: '20 de noviembre', numero: 5 },
+          { nombre: '15 de septiembre', numero: 3 },
+          { nombre: '24 de diciembre', numero: 2 }
+        ]
+      },
+      
+      {
+        nombre: '¿Qué monumento es conocido como "La dama de hierro"?',
+        respuestas: [
+          { nombre: 'Torre Eiffel', numero: 70 },
+          { nombre: 'Estatua de la Libertad', numero: 15 },
+          { nombre: 'Big Ben', numero: 10 },
+          { nombre: 'Torre de Pisa', numero: 3 },
+          { nombre: 'Kremlin', numero: 2 }
+        ]
+      }
+    ]
+  }
 ];
